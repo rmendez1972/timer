bfbc627f5f1096d4855543c8101d089e2ef929c7
Rafael Mendez Asencio <rmendez1972@hotmail.com>
2016-08-25 09:34:48 -0500

    adding initial logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,46 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: 'app-root',
+  selector: 'timer',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  minutos: number;
+  segundos: number;
+  buttonLabel: string;
+  isPaused: boolean;
+
+  constructor() {
+  	this.resetTimer();
+  	setInterval (()=> this.tick(), 1000);
+  }
+
+  private resetTimer(): void {
+  	this.minutos=24;
+  	this.segundos=59;
+  	this.buttonLabel='Empezar';
+  	this.togglePause();
+
+  }
+
+  private tick(): void {
+  	if (this.isPaused){
+  		this.buttonLabel='Detener';
+	  	if(--this.segundos < 0){
+	  		this.segundos=59;
+	  		if (--this.minutos < 0){
+	  			this.resetTimer();
+	  		}
+	  	}
+	}
+  }
+
+  togglePause(): void {
+  	this.isPaused= !this.isPaused;
+  	if (this.minutos<24 || this.segundos<59){
+  		this.buttonLabel= this.isPaused ? 'Reanudar':'Detener';
+  	}
+  }
+
 }
